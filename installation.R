d946422af004728d4f9fe60548f3bfc9a45d8529
ana (also α²) <67763682+anaezquerro@users.noreply.github.com>
2023-07-25 18:05:14 +0200

    update remotes library

--- installation.R
+++ installation.R
@@ -1,9 +1,9 @@
 
 
 # install required packages
-requirements <- c('fpp2', 'tseries', 'TSA', 'forecast', 'seastests', 'forecast', 
+requirements <- c('remotes', 'fpp2', 'tseries', 'TSA', 'forecast', 'seastests', 'forecast', 
                   'libcurl', 'httr', 'plotly', 'prettydoc', 'stringi', 'stringr', 
-                  'polynom', 'parallel', 'Rcpp', 'RcppTOML', 'reticulate')
+                  'polynom', 'parallel')
 installed <- .packages(all.available = TRUE)
 
 for (package in requirements) {
@@ -11,9 +11,10 @@ for (package in requirements) {
         install.packages(package)
     }
 }
+remotes::install_github("rstudio/reticulate")
 
 # create a miniconda environment to save static plotly figures
-reticulate::install_miniconda()
+reticulate::install_miniconda(force=T)
 reticulate::conda_install('r-reticulate', 'python-kaleido==0.1.0')
 reticulate::conda_install('r-reticulate', 'plotly', channel = 'plotly')
 reticulate::use_miniconda('r-reticulate')
